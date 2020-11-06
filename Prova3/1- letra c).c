3ac2f7d3003b5bca18211602aab32d6638eaf485
antoniosa28 <72235344+antoniosa28@users.noreply.github.com>
2020-11-05 22:15:55 -0300

    Add files via upload

---
+++ Prova3/1- letra c).c
@@ -0,0 +1,24 @@
+int splice(*List dest,*List source,int pos){
+
+DlNode *begin_source,*end_source,*current;
+current = *dest;
+
+for(int n=0;n!=pos-1;n++){
+	current= *current.next;
+}
+
+DlNode back_node = *current.prev;
+
+	back_node.next = *source;
+DlNode last_element_source = *source;
+	last_element_source.prev = *back_node;
+
+while(last_element_source.next != null){
+
+	last_element_source= last_element.next;
+}
+
+last_element_source.next = *current;
+current_prev = *last_element;
+
+}
